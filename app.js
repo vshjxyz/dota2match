36455942afeb2921dcc12efede0ff7bf19621749
Luca Del Bianco <vshjxyz@gmail.com>
2014-02-02 19:27:59 +0100

    Small fixes, I need to base everything on the MEAN stack to speed up some results :0

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-
 /**
  * Module dependencies.
  */
-
-var express = require('express');
-var routes = require('./routes');
-var hero = require('./routes/hero');
-var http = require('http');
-var path = require('path');
-var steamApi = require('./modules/steamApi');
-
-var app = express();
+var express = require('express')
+    ,routes = require('./routes')
+    ,hero = require('./routes/hero')
+    ,http = require('http')
+    ,path = require('path')
+    ,steamApi = require('./modules/steamApi')
+    ,app = express()
+;
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -34,8 +32,11 @@ app.get('/', routes.index);
 app.get('/heroes', hero.findAll);
 app.get('/heroes/:id', hero.findById);
 
-steamApi.syncDota2Heroes('en_us');
-steamApi.syncDota2HeroesImages('lg');
+steamApi.syncDota2Heroes('en_us').then(function () {
+    steamApi.syncDota2HeroesImages('lg');
+}, function (errorMsg) {
+    console.error(errorMsg);
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- modules/steamApi.js
+++ modules/steamApi.js
@@ -1,62 +1,66 @@
 var req = require('./getJson')
-    ,mongoose = require('mongoose')
     ,db = require('./db')
     ,fs = require('fs')
-    ,q = require('q')
+    ,Q = require('q')
     ,http = require('http-get')
     ;
 
 exports.syncDota2Heroes = function (language) {
+    var deferred = Q.defer();
     // Drops an error if the language is none of the expected
     if (['en_us'].indexOf(language) === -1) {
-        console.error('You must specify a valid language parameter to sync the heroes!');
-        return;
+        deferred.reject('You must specify a valid language parameter to sync the heroes!');
+    } else {
+        // Performs the AJAX call with the API key
+        req.getJSON({
+            host: 'api.steampowered.com'
+            ,path: '/IEconDOTA2_570/GetHeroes/v0001/?key=8DC022061B9B634134F0196A61A46ED5&language=' + language
+            ,port: 80
+            ,method: 'GET'
+            ,headers: {
+                'Content-Type': 'application/json'
+            }
+        }, function (statusCode, response) {
+            if (statusCode === 200) {
+                var heroes = response.result.heroes.map(function (item) {
+                    // Removing the npc_dota_hero_ prefix
+                    var clean_name = item.name.slice(14);
+                    return {
+                        hero_id: item.id
+                        ,name: item.name
+                        ,eng_name: item.localized_name
+                        ,shown: true
+                        ,img_partial_url: 'http://media.steampowered.com/apps/dota2/images/heroes/' + clean_name
+                    };
+                });
+
+                db.dota2Hero.count({}, function( err, count){
+                    if (count < heroes.length) {
+                        console.log('Removing all the heroes from the database');
+                        db.dota2Hero.remove({}, function (err) {
+                            if (err) {
+                                deferred.reject('Error during the heroes sync (remove)');
+                            } else {
+                                console.log('Creating the new heroes document on the database');
+                                db.dota2Hero.create(heroes, function (err) {
+                                    if (err) {
+                                        deferred.reject('Error during the heroes sync (insert)');
+                                    } else {
+                                        console.log('Synched ' + heroes.length + ' heroes');
+                                        deferred.resolve();
+                                    }
+                                });
+                            }
+                        });
+                    } else {
+                        console.log('There are no new heroes, skipping the sync');
+                        deferred.resolve();
+                    }
+                });
+            }
+        });
     }
-
-    // Performs the AJAX call with the API key
-    req.getJSON({
-        host: 'api.steampowered.com'
-        ,path: '/IEconDOTA2_570/GetHeroes/v0001/?key=8DC022061B9B634134F0196A61A46ED5&language=' + language
-        ,port: 80
-        ,method: 'GET'
-        ,headers: {
-            'Content-Type': 'application/json'
-        }
-    }, function (statusCode, response) {
-        if (statusCode === 200) {
-            var heroes = response.result.heroes.map(function (item) {
-                // Removing the npc_dota_hero_ prefix
-                var clean_name = item.name.slice(14);
-                return {
-                    hero_id: item.id
-                    ,name: item.name
-                    ,eng_name: item.localized_name
-                    ,shown: true
-                    ,img_partial_url: 'http://media.steampowered.com/apps/dota2/images/heroes/' + clean_name
-                };
-            });
-
-            db.dota2Hero.count({}, function( err, count){
-                if (count < heroes.length) {
-                    console.log('Removing all the heroes from the database');
-                    db.dota2Hero.remove({}, function (err) {
-                        if (err) {
-                            console.error('Error during the heroes sync (remove)');
-                        } else {
-                            console.log('Creating the new heroes document on the database');
-                            db.dota2Hero.create(heroes, function (err) {
-                                if (err) {
-                                    console.error('Error during the heroes sync (insert)');
-                                }
-                            });
-                        }
-                    });
-                } else {
-                    console.log('There are no new heroes, skipping the sync');
-                }
-            });
-        }
-    });
+    return deferred.promise;
 };
 
 exports.syncDota2HeroesImages = function (option) {
@@ -65,6 +69,8 @@ exports.syncDota2HeroesImages = function (option) {
         console.error('You must specify a valid image size to sync the heroes!');
         return;
     }
+
+    // Cycles through every hero on the database and downloads his picture if it's not there
     db.dota2Hero.find().exec(function(err, collection) {
         if (collection.length > 0) {
             var downloadImage = function (index) {
@@ -86,7 +92,6 @@ exports.syncDota2HeroesImages = function (option) {
                     downloadImage(index + 1);
                 }
             };
-
             downloadImage(0);
         } else {
             console.log('There are no heroes on the database');
